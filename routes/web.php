f90b56bc49c1cf0fcbd29f4798a9c93ca583f28e
Elmehdi <93981356+Raikonerr@users.noreply.github.com>
2022-04-06 04:10:03 +0000

    completed

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
-
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('products', ProductController::class);
